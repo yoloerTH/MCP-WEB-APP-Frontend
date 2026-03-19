e679e064b446f3c995e1dca270339fcc259dfaf2
yoloerTH <amathelw2@gmail.com>
2026-03-19 20:14:50 +0200

    Add files via upload

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -173,15 +173,6 @@ export default function LandingPage({ onGetStarted }: LandingPageProps) {
         />
       </motion.div>
 
-      {/* Noise texture overlay */}
-      <div className="fixed inset-0 pointer-events-none opacity-[0.015] mix-blend-overlay">
-        <svg className="w-full h-full">
-          <filter id="noise">
-            <feTurbulence type="fractalNoise" baseFrequency="0.8" numOctaves="4" stitchTiles="stitch"/>
-          </filter>
-          <rect width="100%" height="100%" filter="url(#noise)"/>
-        </svg>
-      </div>
 
       {/* Navigation */}
       <motion.nav
